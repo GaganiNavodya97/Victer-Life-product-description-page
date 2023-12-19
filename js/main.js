d98603e507141bb2ddb4c0c74345eeb425d7f652
Gagani <lpgagani97@gmail.com>
2023-12-19 20:24:07 +0530

    complete product discription page

--- js/main.js
+++ js/main.js
@@ -15,3 +15,49 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+// product counter
+$(document).ready(function() {
+    $('.minus').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.plus').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
+});
+
+// toggle tabs
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.getElementById("options").children;
+  
+    for (const button of buttons) {
+      button.addEventListener("click", function () {
+        // Remove 'active' class from all buttons
+        for (const btn of buttons) {
+          btn.classList.remove("active");
+        }
+  
+        // Add 'active' class to the clicked button
+        this.classList.add("active");
+  
+        // Hide all content divs
+        const contentDivs = document.querySelectorAll(".content");
+        for (const contentDiv of contentDivs) {
+          contentDiv.style.display = "none";
+        }
+  
+        // Display the corresponding content div
+        const contentId = this.id.replace("option", "content-option");
+        const contentToShow = document.getElementById(contentId);
+        contentToShow.style.display = "block";
+      });
+    }
+  });
+  
